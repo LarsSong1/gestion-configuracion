23e6b85d8b2f57fd15b9004c3006ada944f24e68
Sindy Parra <sparrav@unemi.edu.ec>
2025-06-06 19:40:38 -0500

    Aquí escribe tu mensaje de commit

--- gestionc/src/app/page.js
+++ gestionc/src/app/page.js
@@ -3,7 +3,7 @@ export default function Home() {
     <main className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-3xl font-bold">Jair Gavilanez</h1>
       <h1 className="text-3xl font-bold">Bryan Anchundia</h1>
-      <h1 className="text-3xl font-bold">Sindy Parra</h1>
+      <h1 className="text-3xl font-bold">Sindy Parra V.</h1>
     </main>
   );
 }
